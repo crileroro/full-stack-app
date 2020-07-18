3eb821ac1f51e61eda36c839bd2a541a2350bb55
Cristhian Roa <cristhian@designisdead.com>
2020-07-18 19:08:00 +0200

    Fixed typo in endpoints

--- backend/app.py
+++ backend/app.py
@@ -7,8 +7,8 @@ from resources import Item, ItemList
 app = Flask(__name__)
 
 api = Api(app)
-api.add_resource(Item, '/app/item/<string:name>')
-api.add_resource(ItemList, '/app/items-all')
+api.add_resource(Item, '/api/item/<string:name>')
+api.add_resource(ItemList, '/api/items-all')
 
 if __name__ == "__main__":
     app.run(host= env_host(), port=env_port(), debug=env_debug())
